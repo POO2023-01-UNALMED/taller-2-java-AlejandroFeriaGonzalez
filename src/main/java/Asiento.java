f24e575375005d5358e93de97ce3bcd689749387
alejandro20019 <aferiag@unal.edu.co>
2023-02-14 21:36:00 -0500

    definiendo atributos y metodos

---
+++ src/main/java/Asiento.java
@@ -0,0 +1,10 @@
+// package test;
+
+public class Asiento {
+    public String color;
+    public int precio, registro;
+
+    public void cambiarColor(String color) {
+
+    }
+}
---
+++ src/main/java/Motor.java
@@ -0,0 +1,14 @@
+// package test;
+
+public class Motor {
+    public int numeroCilindros, registro;
+    public String tipo;
+
+    public void cambiarRegistro(int registro) {
+
+    }
+
+    public void asignarTipo(String tipo) {
+        
+    }
+}
