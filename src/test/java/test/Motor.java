b42897f29b71990b43ecca710053a00c56030661
alejandro20019 <aferiag@unal.edu.co>
2023-02-16 18:52:03 -0500

    pasando trabajo a carpeta test

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,14 +1,16 @@
-// package test;
+package test;
 
 public class Motor {
     public int numeroCilindros, registro;
     public String tipo;
 
     public void cambiarRegistro(int registro) {
-
+        this.registro = registro;
     }
 
     public void asignarTipo(String tipo) {
-        
+        if (tipo == "electrico" || tipo == "gasolina"){
+            this.tipo = tipo;
+        }
     }
 }
