cc1f1022fbeff64139bc9894cb91074444ddbb5d
alejandro20019 <aferiag@unal.edu.co>
2023-02-16 18:54:08 -0500

    volviendo archivos a carpeta main

---
+++ src/main/java/Asiento.java
@@ -0,0 +1,11 @@
+
+public class Asiento {
+    public String color;
+    public int precio, registro;
+
+    public void cambiarColor(String color) {
+        if (color == "rojo" || color == "verde" || color == "amarillo" ||color == "negro" ||color == "blanco"){
+            this.color = color;
+        }
+    }
+}
---
+++ src/main/java/Auto.java
@@ -0,0 +1,24 @@
+
+
+public class Auto {
+    public String modelo, marca;
+    public int precio, registro;
+    public Asiento[] asientos;
+    public Motor motor;
+    public static int cantidadCreados;
+
+    public int cantidadAsientos(){
+        return this.asientos.length;
+    }
+
+    public String verificarIntegridad() {
+        for (Asiento asiento : asientos) {
+            if (asiento.registro == this.registro && asiento.registro  == motor.registro){
+                continue;
+            } else {
+                return "Las piezas no son originales";
+            }
+        }
+        return "Auto original";
+    }
+}
